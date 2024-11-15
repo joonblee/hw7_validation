836ef4055f43b6312dc034c0df53be46b5ba83c6
taehee <eikz.il2ljlpl@gmail.com>
2024-11-15 16:08:05 +0900

    get zpmass and gbb value as arguments

--- FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_hw.sh
+++ FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_hw.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-#mg_job_numbers=("2491183_50_80" "2491184_80_120" "2491185_120_170" "2491186_170_300" "2491187_300_470" "2491188_470_600" "2491189_600_800" "2491190_800_1000" "2491191_1000_9999")
+SAMPLES=("Pt-60To65_1366782" "Pt-65To70_1366783" "Pt-70To75_1366784" "Pt-75To80_1366785" "Pt-80To85_1366786" "Pt-85To90_1366787" "Pt-90To100_1366788" "Pt-100To140_1366789" "Pt-140To200_1366790" "Pt-200To9999_1366792")
+
+zpmass=5
+coupling="0.05"
 
 cat <<EOT > submit_condor_hw.txt
 universe        = vanilla
 executable      = hw_condor.sh
-arguments       = \$(Cluster) \$(Process) \$(mg_job_number)
+arguments       = \$(Cluster) \$(Process) \$(sample) \$(zpmass) \$(coupling)
 output          = job.\$(Cluster).\$(Process).out
 error           = job.\$(Cluster).\$(Process).err
 log             = job.\$(Cluster).\$(Process).log
 +SingularityImage = "/data6/Users/taehee/herwig_sandbox"
 +SingularityBind  = "/data6/Users/taehee/HerwigWD:/data6/Users/taehee/HerwigWD"
-
-queue 100
 EOT
 
 # Loop through the array and submit jobs with adjacent elements
-for ((i=0; i<${#mg_job_numbers[@]}; i++)); do
-    mg_job_number=${mg_job_numbers[i]}
-    echo "Submitting job for HW with MG job number $mg_job_number"
-    condor_submit submit_condor_hw.txt -append "arguments = \$(Cluster) \$(Process) $mg_job_number"
-    sleep 60
+for ((i=0; i<${#SAMPLES[@]}; i++)); do
+    sample=${SAMPLES[i]}
+    queue=$(ls /gv0/Users/taehee/HerwigSample/mg/MZp-$zpmass/$sample | wc -l)
+    echo "Submitting job for HW with MG job number $sample with Zprime mass $zpmass, coupling $coupling"
+    condor_submit submit_condor_hw.txt \
+    -append "arguments = \$(Cluster) \$(Process) $sample $zpmass $coupling" \
+    -append "JobBatchName = MZp-"$zpmass"_"$sample \
+    -append "queue $queue"
+    sleep 5
 done
