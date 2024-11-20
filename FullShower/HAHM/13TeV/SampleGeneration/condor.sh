95ba6b73b8cab0353fc4ead39bb37f466bf1911e
taehee <eikz.il2ljlpl@gmail.com>
2024-11-20 21:40:52 +0900

    get zpmass as an argument

--- FullShower/HAHM/13TeV/SampleGeneration/condor.sh
+++ FullShower/HAHM/13TeV/SampleGeneration/condor.sh
@@ -3,6 +3,7 @@
 process=${1}
 sample=${2}
 campaign=${3}
+zpmass=${4}
 
 if [ "$campaign" == "RunIISummer20UL16" ]; then
     CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_8_0_36_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_25")
@@ -24,16 +25,16 @@ export SCRAM_ARCH=slc7_amd64_gcc700
 
 WD="/data6/Users/taehee/HerwigWD/hw7_validation/FullShower/HAHM/13TeV/SampleGeneration"
 outputdir="\/gv0\/Users\/taehee\/HerwigSample"
-mkdir -p /gv0/Users/taehee/HerwigSample/samples/${campaign}/${sample}
-mkdir -p tmp/${campaign}/${sample}
+mkdir -p /gv0/Users/taehee/HerwigSample/samples/${campaign}/MZp-${zpmass}/${sample}
+mkdir -p tmp/${campaign}/MZp-${zpmass}/${sample}
 
 for ((i = 0; i < ${#RUNS[@]}; i++)); do
     output=${RUNS[$i]}
-    if [ -s "/gv0/Users/taehee/HerwigSample/samples/${campaign}/${sample}/${output}_${process}.root" ];then
-        echo "/gv0/Users/taehee/HerwigSample/samples/${campaign}/${sample}/${output}_${process}.root: File exists... pass"
+    if [ -s "/gv0/Users/taehee/HerwigSample/samples/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.root" ];then
+        echo "/gv0/Users/taehee/HerwigSample/samples/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.root: File exists... pass"
         continue
     fi
-    
+
     if [[ $output != "GEN" ]]; then
         input=${RUNS[$((i-1))]}
     fi
@@ -41,13 +42,13 @@ for ((i = 0; i < ${#RUNS[@]}; i++)); do
     eval `scram runtime -sh`
     scram b
     cd $WD
-    sed -e "s/__OUTPUT__/${outputdir}\/samples\/${campaign}\/${sample}\/${output}_${process}/g" "files_cfg/${campaign}${output}_cfg.py" > "tmp/${campaign}/${sample}/${output}_${process}.py"
+    sed -e "s/__OUTPUT__/${outputdir}\/samples\/${campaign}\/MZp-${zpmass}\/${sample}\/${output}_${process}/g" "files_cfg/${campaign}${output}_cfg.py" > "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
     if [[ $output == "GEN" ]]; then
-        sed -i "s/__INPUT__/${outputdir}\/hw\/${sample}\/$process\/LHC/g" "tmp/${campaign}/${sample}/${output}_${process}.py"
-        sed -i "s/__RANDOM__/${process}/g" "tmp/${campaign}/${sample}/${output}_${process}.py"
+        sed -i "s/__INPUT__/${outputdir}\/hw\/MZp-${zpmass}\/${sample}\/$process\/LHC/g" "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
+        sed -i "s/__RANDOM__/${process}/g" "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
     else
-        sed -i "s/__INPUT__/${outputdir}\/samples\/${campaign}\/${sample}\/${input}_${process}/g" "tmp/${campaign}/${sample}/${output}_${process}.py"
+        sed -i "s/__INPUT__/${outputdir}\/samples\/${campaign}\/MZp-${zpmass}\/${sample}\/${input}_${process}/g" "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
     fi
-    cmsRun tmp/${campaign}/${sample}/${output}_${process}.py &> tmp/${campaign}/${sample}/${output}_${process}.log
+    cmsRun tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py &> tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.log
 
 done
