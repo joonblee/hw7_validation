622f59c4b070578ac387781ecaa38d1136ad3b05
taehee <eikz.il2ljlpl@gmail.com>
2024-12-06 16:00:35 +0900

    compile in data6, not gv0. change the setting for gbs

--- FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/hw_condor.sh
+++ FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/hw_condor.sh
@@ -20,6 +20,9 @@ sample=${3}
 ZprimeMass=${4}
 Coupling=${5}
 
+outputdir=/gv0/Users/taehee/HerwigSample/hw/MZp-${ZprimeMass}/${sample}
+WD=${Hw_Loc}/hw7_validation/FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/tmp/MZp-${ZprimeMass}/${sample}/${2}
+
 # Herwig7 basic setups
 #ln -s $(which python3) $Singularity_Loc/.local/bin/python
 export PATH=$Singularity_Loc/.local/bin:$PATH
@@ -42,13 +45,11 @@ export PKG_CONFIG_PATH="$Singularity_Loc/.local/lib/pkgconfig"
 ### run ###
 ###########
 
-outputdir=/gv0/Users/taehee/HerwigSample/hw/MZp-${ZprimeMass}/${sample}/${2}
 echo "Make a run directory, $outputdir"
-
-rm -rf ${outputdir}
 mkdir -p ${outputdir}
-cp $Hw_Loc/hw7_validation/FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/RAnalysis.cc ${outputdir}
-cd ${outputdir}
+mkdir -p ${WD}
+cd ${WD}
+cp $Hw_Loc/hw7_validation/FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/RAnalysis.cc ${WD}
 
 RB="$Hw_Loc/bin/rivet-build"
 source "$Hw_Loc/bin/activate"
@@ -56,9 +57,10 @@ source "$Hw_Loc/bin/activate"
 # compiling ufo file
 if [ "$CompileUFO" = true ] && [ ! -f FRModel.model ]; then
   if [[ ! -d ${UFOName} ]]; then
-    cp -r /gv0/Users/taehee/HerwigSample/feynrules-current/Models/RKZp/RKZp_UFO .
+    cp -r /gv0/Users/taehee/HerwigSample/feynrules-current/Models/RKZp/RKZp_UFO ${WD}
     sed -i "127s/10./${ZprimeMass}/" ${UFOName}/parameters.py
     sed -i "23s/1./${Coupling}/" ${UFOName}/parameters.py #gbb
+    sed -i "187s/0.04*gbb/0.0/" ${UFOName}/parameters.py #gbs
   fi
   ufo2herwig ${UFOName} --enable-bsm-shower
   sed -i "s/echo \*.cc/echo FRModel*.cc/g" Makefile
@@ -71,15 +73,15 @@ fi
 echo "Compile the rivet analysis, RAnalysis.cc"
 chmod +x $RB
 $RB Rivet.so RAnalysis.cc
-export RIVET_ANALYSIS_PATH=$outputdir
+export RIVET_ANALYSIS_PATH=$outputdir/$2
 echo ""
 
 # run hw7
 echo "Start runnning LHC.${1}.${2} (mg job # = ${sample})"
 rnum=$(shuf -i 1-99999999 -n 1)
-sed -e "s/__NEVENTS__/${EVTpRUN}/g" ${Hw_Loc}/hw7_validation/FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/LHC.in > ${outputdir}/LHC.in
-sed -i "s/__SEED__/${rnum}/g" "${outputdir}/LHC.in"
-sed -i "s/__DIR__/\/gv0\/Users\/taehee\/HerwigSample\/mg\/MZp-${ZprimeMass}\/${sample}\/${2}/g" "${outputdir}/LHC.in"
+sed -e "s/__NEVENTS__/${EVTpRUN}/g" ${Hw_Loc}/hw7_validation/FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/LHC.in > LHC.in
+sed -i "s/__SEED__/${rnum}/g" LHC.in
+sed -i "s/__DIR__/\/gv0\/Users\/taehee\/HerwigSample\/mg\/MZp-${ZprimeMass}\/${sample}\/${2}/g" LHC.in
 if [ "$ZprimeMass" -lt 9 ];then
     sed -i '37s/^/#/' LHC.in
 fi
@@ -88,6 +90,8 @@ if [ "$ZprimeMass" -lt 4 ];then
     sed -i '43,44s/^/#/' LHC.in
 fi
 
+mv ${WD} ${outputdir}
+cd ${outputdir}/${2}
 Herwig read LHC.in 
 Herwig run LHC.run 
 
