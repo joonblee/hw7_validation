b7368c6895b0bfde745cd926c0d41a7410e7bf55
taehee <eikz.il2ljlpl@gmail.com>
2024-12-06 16:03:09 +0900

    fix minor things

--- FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_hw.sh
+++ FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_hw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SAMPLES=("Pt-60To65_1366782" "Pt-65To70_1366783" "Pt-70To75_1366784" "Pt-75To80_1366785" "Pt-80To85_1366786" "Pt-85To90_1366787" "Pt-90To100_1366788" "Pt-100To140_1366789" "Pt-140To200_1366790" "Pt-200To9999_1366792")
+SAMPLES=("Pt-65To70_1459323" "Pt-70To75_1459324" "Pt-75To80_1459325" "Pt-80To90_1459326" "Pt-90To100_1459327" "Pt-100To120_1459328" "Pt-120To150_1459329" "Pt-150To9999_1459330")
 
 zpmass=5
 coupling="0.05"
@@ -9,11 +9,13 @@ cat <<EOT > submit_condor_hw.txt
 universe        = vanilla
 executable      = hw_condor.sh
 arguments       = \$(Cluster) \$(Process) \$(sample) \$(zpmass) \$(coupling)
-output          = job.\$(Cluster).\$(Process).out
-error           = job.\$(Cluster).\$(Process).err
-log             = job.\$(Cluster).\$(Process).log
+output          = joblog/job.\$(Cluster).\$(Process).out
+error           = joblog/job.\$(Cluster).\$(Process).err
+log             = joblog/job.log
 +SingularityImage = "/data6/Users/taehee/herwig_sandbox"
 +SingularityBind  = "/data6/Users/taehee/HerwigWD:/data6/Users/taehee/HerwigWD"
+stream_output = True
+stream_error = True
 EOT
 
 # Loop through the array and submit jobs with adjacent elements
@@ -25,5 +27,4 @@ for ((i=0; i<${#SAMPLES[@]}; i++)); do
     -append "arguments = \$(Cluster) \$(Process) $sample $zpmass $coupling" \
     -append "JobBatchName = MZp-"$zpmass"_"$sample \
     -append "queue $queue"
-    sleep 5
 done
--- FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_mg.sh
+++ FullShower/HAHM/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_mg.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ptbin=(65 70 75 80 85 90 100 140 200 9999)
-queues=(317 153 76 39 20 37 69 36 0)
+ptbin=(65 70 75 80 90 100 120 150 9999)
 
 zpmass=5
 
@@ -9,9 +8,9 @@ cat <<EOT > submit_condor_mg.txt
 universe        = vanilla
 executable      = mg_condor.sh
 arguments       = \$(Cluster) \$(Process) \$(ptj) \$(ptjmax) \$(zpmass)
-output          = job.\$(Cluster).\$(Process).out
-error           = job.\$(Cluster).\$(Process).err
-log             = job.\$(Cluster).\$(Process).log
+output          = joblog/job.\$(Cluster).\$(Process).out
+error           = joblog/job.\$(Cluster).\$(Process).err
+log             = joblog/job.log
 +SingularityImage = "/data6/Users/taehee/herwig_sandbox"
 +SingularityBind = "/data6/Users/taehee/HerwigWD:/data6/Users/taehee/HerewigWD"
 should_transfer_files = YES
@@ -21,7 +20,7 @@ for ((i=0; i<${#ptbin[@]}-1; i++)); do
     ptj=${ptbin[i]}
     ptjmax=${ptbin[i+1]}
     queue=${queues[i]}
-    #queue=30
+    queue=30
     echo "Submitting job for MG ptbinned [$ptj, $ptjmax] with Zprime mass $zpmass"
     condor_submit submit_condor_mg.txt \
     -append "arguments = \$(Cluster) \$(Process) $ptj $ptjmax $zpmass" \
