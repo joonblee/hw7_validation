7ac1e068ccd2d86038d69c3b25c8049de3a9e273
taehee <eikz.il2ljlpl@gmail.com>
2024-12-16 17:58:45 +0900

    capture the error from filter.py

--- FullShower/RKZp/13TeV/QCD_M5_13TeV-mg-hw/filter.py
+++ FullShower/RKZp/13TeV/QCD_M5_13TeV-mg-hw/filter.py
@@ -1,33 +1,41 @@
-import pyhepmc as hep
-import pyhepmc.io as hep_io
+import subprocess
+import os
 
 input_file = "LHC.hepmc"
 output_file = "filtered.hepmc"
+print("Start filtering LHC.hepmc file...")
+if not os.path.exists(input_file):
+    print("No LHC.hepmc file. exit")
+    exit()
+if os.path.exists(output_file):
+    os.remove(output_file)
 
-reader = hep.open(input_file, "r")
-writer = hep_io.WriterAsciiHepMC2(output_file)
-#writer = hep.open(output_file, "w")
-
-#Nzp = 0
-
+command = f"""
+python3 -c '
+import pyhepmc as hep
+import pyhepmc.io as hep_io
+reader = hep.open("{input_file}", "r")
+writer = hep_io.WriterAsciiHepMC2("{output_file}")
 for event in reader:
-    # Muon filter
-    #muons = [p for p in event.particles if abs(p.pid) == 13 and p.status == 1]
-    #if len(muons) == 2:  # Look for exactly two final-state muons
-    #    m = invariant_mass(muons[0].momentum, muons[1].momentum)
-    #    if m > 50:  # Keep only if M(mu+, mu-) > 50 GeV
-    #        writer.write(event)
-
-    # Zp filter
     zp = [p for p in event.particles if abs(p.pid) == 32]
     if len(zp) > 0:
         writer.write(event)
-        #Nzp += 1
-        #print("Event with Z' - ID:", event.event_number)
-        #if Nzp >= 10:
-        #    break
-
 reader.close()
 writer.close()
-print(f"Filtered events saved to {output_file}")
+print("Filtered events saved to {output_file}")
+'
+"""
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+print(result.stdout)
+print(result.stderr)
 
+if "ERROR" in result.stdout or "ERROR" in result.stderr:
+    print("Error detected. Delete filtered.hepmc.")
+    if os.path.exists(output_file):
+        os.remove(output_file)
+else:
+    print(f"Filtered events saved to {output_file}.")
+
+print("Delete LHC.hepmc to free up memory")
+if os.path.exists(input_file):
+    os.remove(input_file)
